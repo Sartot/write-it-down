c5d2be0d25f1e997206fd9788153fbdf3649cd1f
Sartot <slorenzo.sartori@protonmail.com>
2025-04-16 23:09:55 +0200

    refactor: optimize note fetching with caching mechanism

--- app/notes/[[...id]]/layout.js
+++ app/notes/[[...id]]/layout.js
@@ -2,6 +2,17 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/ui/app-sidebar";
 import { auth } from "@/lib/auth"
 import { headers } from 'next/headers'
+import { unstable_cache } from 'next/cache';
+
+const getCachedNotes = unstable_cache(
+    async (userId) => {
+        return fetch(process.env.URL+'/api/notes?user_id='+userId, {
+            method: "GET",
+        }).then(res => res.json());
+    },
+    ['notes-data'],
+    { revalidate: 60 } // Riconvalida ogni minuto
+);
 
 export default async function DashboardLayout({children, params}){
     const { id } = await params;
@@ -11,14 +22,7 @@ export default async function DashboardLayout({children, params}){
             headers: await headers(), // pass the headers
         }); 
 
-        return fetch(process.env.URL+'/api/notes?user_id='+session.userId, {
-            method: "GET",
-            cache: 'force-cache'
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            return data;
-        })
+        return getCachedNotes(session.userId);
     }
 
     const notes = await fetchNotes();
